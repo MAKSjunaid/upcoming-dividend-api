f4751feabb208ba8ad440dfe366b3599e5255371
MAKSjunaid <134540767+MAKSjunaid@users.noreply.github.com>
2026-09-01 15:18:44 +0530

    Update MockData.java

--- src/main/java/com/example/upcomingdividendapi/MockData.java
+++ src/main/java/com/example/upcomingdividendapi/MockData.java
@@ -9,7 +9,7 @@ import java.util.Locale;
 public class MockData {
 
     // ============================================================
-    // DATE FORMATTERS
+    // DATE FORMATTERS ok
     // ============================================================
 
     /*
